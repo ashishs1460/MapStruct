f17470faab04ac03dfa80c044a013e4b2f546d8d
Ashish <ashish.s@afcdigitalinc.com>
2025-01-23 16:38:20 +0530

    feat: Implemented Custom mapping

---
+++ src/main/java/org/example/customMapping/StudentMapper.java
@@ -0,0 +1,16 @@
+package org.example.customMapping;
+
+import org.example.basicMapping.Student;
+import org.example.basicMapping.StudentEntity;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface StudentMapper {
+    default Student getModelFromEntity(StudentEntity studentEntity){
+        Student student = new Student();
+        student.setId(studentEntity.getId());
+        student.setName(studentEntity.getName());
+        student.setClassName(studentEntity.getClassVal());
+        return student;
+    }
+}
